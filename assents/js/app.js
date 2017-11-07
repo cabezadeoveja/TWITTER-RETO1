41d27fa0a55b38fffc7f41fa7712d7756a80cd52
cabezadeoveja <pabla.bazan@gmail.com>
2017-11-07 00:46:37 -0300

    Versión 0.0.2

--- assents/js/app.js
+++ assents/js/app.js
@@ -1,14 +1,28 @@
+function contar() {
+	//Contar la cantidad de caracteres de forma regresiva.
+	var max = "140";
+	var cadena = document.getElementById("comment").value;
+	var longitud = cadena.length;
+
+		if(longitud <= max) {
+		    document.getElementById("contador").value = max-longitud;
+		}
+		else {
+		    document.getElementById("comment").value = cadena.substr(0, max);
+		}
+	}
+
 function add(){
 //Almacenando valor del tweet
-var comments = document.getElementById("comment").value;
+	var comments = document.getElementById("comment").value;
 	//estoy rescatando lo que el usuario va a escribir
     document.getElementById("comment").value ="";
     //lo igualo a un elemento vacio para que lo limpie del text area al momento de enviar.
     
-// crear un evento para que cuando haga click cambié de color el boton
+	// crear un evento para que cuando haga click cambié de color el boton
 
-    if (comments.lenght == 0 || comments == null){
-    	return document.getElementById('btn').disabled = true;
+   		 if (comments.lenght == 0 || comments == null){
+    			return document.getElementById('btn').disabled=true;
     //para desabilitar el button cuando no se escriba nada, esot lo hacemos mediante disabled.
     }
 
@@ -19,7 +33,6 @@ var comments = document.getElementById("comment").value;
 
 
     var paragraph = document.createElement('p');
-    paragraph.classList.add ('color');
     //crear una etiqueta P con una clase. P tiene un hijo que es un nodo de texto.
     //vamos a crear un nodo de texto.
     var nodoText = document.createTextNode(comments);
@@ -32,4 +45,6 @@ var comments = document.getElementById("comment").value;
 
 };
 
+
+	
 
